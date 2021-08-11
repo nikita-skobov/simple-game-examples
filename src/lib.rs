69ad89ebd81643240ef91f90d60f545de2d7d42e
nikita-skobov <nskobov.websites@gmail.com>
2021-08-10 19:45:12 -0500

    adds bpp field to Canvas, adds draw horiz/vert line API

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 pub mod backend;
 
+#[derive(Default)]
 pub struct Canvas {
     pub width: usize,
     pub height: usize,
     data: Vec<u8>,
+    pub bpp: usize,
 }
 
 #[derive(Debug, Default, Copy, Clone)]
@@ -24,30 +26,28 @@ impl Rgb {
 
 macro_rules! get_red_index {
     ($me:tt, $i:tt, $j:tt) => {
-        ($i * Canvas::BPP) + ($j * $me.span())
+        ($i * $me.bpp) + ($j * $me.span())
     };
 }
 
 impl Canvas {
-    // bytes per pixel, always 3. we dont consider alphas here...
-    const BPP: usize = 3;
-
-    pub fn new(width: usize, height: usize) -> Canvas {
-        let data = vec![0; width * height * Canvas::BPP];
+    pub fn new(width: usize, height: usize, bpp: usize,) -> Canvas {
+        let data = vec![0; width * height * bpp];
         Canvas {
             data,
             width,
             height,
+            bpp,
         }
     }
 
     #[inline(always)]
     pub fn span(&self) -> usize {
-        self.width * Canvas::BPP
+        self.width * self.bpp
     }
 
-    pub fn new_with_color(width: usize, height: usize, color: Rgb) -> Canvas {
-        let mut canvas = Canvas::new(width, height);
+    pub fn new_with_color(width: usize, height: usize, color: Rgb, bpp: usize) -> Canvas {
+        let mut canvas = Canvas::new(width, height, bpp);
         canvas.fill(color);
         canvas
     }
@@ -56,7 +56,7 @@ impl Canvas {
         let span = self.span();
         for i in 0..self.width {
             for j in 0..self.height {
-                let x_offset = i * Canvas::BPP;
+                let x_offset = i * self.bpp;
                 let y_offset = j * span;
                 let red_index = x_offset + y_offset;
                 self.set_pixel_from_index(red_index, color);
@@ -66,6 +66,7 @@ impl Canvas {
 
     /// red_index should be the index of the data vector for
     /// a red value of the pixel we wish to set.
+    #[inline(always)]
     pub fn set_pixel_from_index(&mut self, red_index: usize, color: Rgb) {
         self.data[red_index] = color.red;
         self.data[red_index + 1] = color.green;
@@ -79,5 +80,25 @@ impl Canvas {
 
     pub fn into_raw(self) -> (usize, usize, Vec<u8>) {
         (self.width, self.height, self.data)
+    }
+
+    pub fn draw_horizontal_line(&mut self, y: usize, x1: usize, x2: usize, color: Rgb) {
+        let span = self.span();
+        let y_offset = y * span;
+        for i in x1..x2 {
+            let x_offset = i * self.bpp;
+            let red_index = x_offset + y_offset;
+            self.set_pixel_from_index(red_index, color);
+        }
+    }
+
+    pub fn draw_vertical_line(&mut self, x: usize, y1: usize, y2: usize, color: Rgb) {
+        let span = self.span();
+        let x_offset = x * self.bpp;
+        for j in y1..y2 {
+            let y_offset = j * span;
+            let red_index = x_offset + y_offset;
+            self.set_pixel_from_index(red_index, color);
+        }
     }
 }
